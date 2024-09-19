cf93d8e022dea58d8ce17d375190e60b5f4e542c
Malachai Vargas <mvarg712stu.southtexascollege.edu>
2024-09-18 21:46:40 -0500

    first commit of all the files

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import {ref} from 'vue'
+
+const name = red('')
+
+
+</script>
+
+<template>
+
+<main class="app">
+
+<section class ="greeting">
+  <h2 class="title">
+   Welcome back, <input type="text" placeholder="Enter Name" v-model="name">
+</h2>
+</section>
+
+<section class="create-todo">
+
+</section>
+
+<section class="todo-list">
+
+</section>
+
+</main>
+
+</template>
+
+
+
+
+
